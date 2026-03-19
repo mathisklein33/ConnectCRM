efe448a869c530ea07e951956462363322216a26
mathisklein33 <mathisklein33@gmail.com>
2026-03-19 14:47:01 +0100

    création de pdf via la bibliothéque DomPDF (#15)

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,68 @@
+﻿<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invoice</title>
+
+    <link rel="stylesheet" href="{{ public_path('css/pdf.css') }}">
+</head>
+<body>
+
+    <table class="w-full">
+         <tr>
+             <td class="w-half">
+                 <img src="{{ public_path('laraveldaily.png') }}" alt="laravel daily" width="200">
+              </td>
+             <td class="w-half">
+                 <h2>Invoice ID: 834847473</h2>
+             </td>
+         </tr>
+    </table>
+
+    <div class="margin-top">
+        <table class="w-full">
+            <tr>
+                <td class="w-half">
+                    <div><h4>To:</h4></div>
+                    <div>John Doe</div>
+                    <div>123 Acme Str.</div>
+                </td>
+                <td class="w-half">
+                    <div><h4>From:</h4></div>
+                    <div>Laravel Daily</div>
+                    <div>London</div>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="margin-top">
+        <table class="products">
+            <tr>
+                <th>Qty</th>
+                <th>Description</th>
+                <th>Price</th>
+            </tr>
+
+            @foreach($data as $item)
+                <tr class="items">
+                    <td>{{ $item['quantity'] }}</td>
+                    <td>{{ $item['description'] }}</td>
+                    <td>{{ $item['price'] }}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+
+    <div class="total">
+        Total: $129.00 USD
+    </div>
+
+    <div class="footer margin-top">
+        <div>Thank you</div>
+        <div>&copy; Laravel Daily</div>
+    </div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\InteractionController;
+use App\Http\Controllers\PdfController;
 
+
+Route::get('/pdf/download', [PdfController::class, 'download']);
 
 Route::get('/', function () {
     return view('welcome');
