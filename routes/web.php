6ec0bddf141ccb96685e56190c977d0692d813d3
Mathieu Leboucher <138926928+Coolmathieu@users.noreply.github.com>
2026-03-27 10:02:12 +0100

    Create products opportunity (#44)
    * .Planification de rendez-vous eamil et appels
    
    £Les autre fichier, c'est les ficher ou bom à été enlever
    
    * .Planification de rendez-vous eamil et appels
    
    £Les autre fichier, c'est les ficher ou bom à été enlever
    
    * création de produit et opportunité
    
    * Update web.php
    
    ---------
    
    Co-authored-by: Mathieu Leboucher <mathieu.leboucher76@gmail.com>
    Co-authored-by: mathisklein33 <mathisklein33@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,7 @@ Route::middleware('auth')->group(function () {
 
     // Commercial
     Route::middleware('role:commercial')->group(function () {
+        Route::resource('opportunity', OpportunityController::class);
         Route::patch('/opportunity/stage/{id}', [OpportunityController::class, 'updateStage'])->name('opportunity.updateStage');
         Route::get('/opportunity/show/{id}', [OpportunityController::class, 'show'])->name('opportunity.show');
 
