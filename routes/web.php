1a2a1fc0a763b518d156ae6438d7abb8f0c08f51
Mathieu Leboucher <mathieu.leboucher76@gmail.com>
2026-03-30 13:46:00 +0200

    pages profile

--- routes/web.php
+++ routes/web.php
@@ -149,7 +149,8 @@ Route::middleware('auth')->group(function () {
     | Profile
     |--------------------------------------------------------------------------
     */
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
